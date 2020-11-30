0cdf0ab5e66e0e6362d348e62a807834d0a0dbd4
sahe417 <41294520+sahe417@users.noreply.github.com>
2020-11-30 10:37:51 +0900

    typo

--- main.js
+++ main.js
@@ -1,34 +1,34 @@
 'use strict';
 
-const fs     = require('fs');
-const read   = require('readline');
-const reader = read.createInterface({ input: fs.createReadStream('./rename.csv')});
+const fs = require('fs');
+const read = require('readline');
+const reader = read.createInterface({ input: fs.createReadStream('./rename.csv') });
 
 // file list
 var filelist = [];
 
-fs.readdir('.', function(err, files){
+fs.readdir('.', function (err, files) {
   if (err) throw err;
-	filelist = files.filter(function(file){
-		return fs.statSync(file).isFile() && /.*\.pdf$/.test(file);
-	})
+  filelist = files.filter(function (file) {
+    return fs.statSync(file).isFile() && /.*\.pdf$/.test(file);
+  })
 });
 
 
-reader.on('line', function(line) {
-	const arr   = line.split(',');
-	var number  = arr[0].trim();
-	var title   = arr[1].replace(/(\\|\*|\"|\?|\<|\>|\:|\/)/g, '_').trim();
-	var newfile = number + '_' + title + '.pdf';
+reader.on('line', function (line) {
+  const arr = line.split(',');
+  var number = arr[0].trim();
+  var title = arr[1].replace(/(\\|\*|\"|\?|\<|\>|\:|\/)/g, '_').trim();
+  var newfile = number + '_' + title + '.pdf';
 
-	//console.log(filelist);
-	filelist.some(function(origfile) {
-		if (origfile.match(number) && fs.statSync(origfile)) {
-			fs.rename(origfile.trim(), newfile.trim(), function(err) {
-				if (err) throw err;
-				console.log("Rename:" + newfile + " OK!!");
-				return true;
-			})
-		}
-	})
+  //console.log(filelist);
+  filelist.some(function (origfile) {
+    if (origfile.match(number) && fs.statSync(origfile)) {
+      fs.rename(origfile.trim(), newfile.trim(), function (err) {
+        if (err) throw err;
+        console.log("Rename:" + newfile + " OK!!");
+        return true;
+      })
+    }
+  })
 })
